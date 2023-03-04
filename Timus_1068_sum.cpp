04c42127d20e6038e89ea827a41cc26afb2ba95c
Shahriar Mahmud Arman <iamarman252575@gmail.com>
2023-03-04 22:48:38 +0600

    Add files via upload

---
+++ Timus_1068_sum.cpp
@@ -0,0 +1,166 @@
+                                          //   Bismillahir Rahmanir Rahim      //
+                                         //      SHAHRIAR MAHMUD ARMAN        //
+                                        //         AUTHOR : iamarman         //
+
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <cassert>
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+
+                                             ////       MACROS       ////
+
+//---------------------------------------------------------------------------------------------------------------------------------|
+
+# define ll long long                                                                                                              
+# define test int test; cin>>test; while(test--)
+# define rep(i,a,b) for(int i=a;i<b;i++)
+# define rep_n(i,a,b) for(int i=a;i<=b;i++)
+# define rep_rev(i,a,b) for(int i=b;a>=1;i--)
+# define pb push_back
+# define el "\n"
+# define optimise   ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); cin.clear(); 
+# define R0 return 0
+# define all(v) v.begin(),v.end()
+# define srt(vvv) sort(vvv.begin(),vvv.end())
+# define sp " "
+# define vii vector<int>
+# define vll vector<long long>
+# define yess cout<<"YES"<<el
+# define noo cout<<"NO"<<el
+# define PI 2*acos(0.0)
+ll mod=10000000+7;
+
+//----------------------------------------------------------------------------------------------------------------------------------|
+
+
+//                                             ///      NAMESPACE     ///
+
+using namespace std;
+
+
+                     /// THIS IS FOR GIVING INPUT AND GETTING OUTPUT IN A FILE INSTEAD OF TERMINAL OR CONSOLE   ///
+
+
+inline void file() {
+
+#ifndef ONLINE_JUDGE
+
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+
+#endif // ONLINE_JUDGE
+}
+
+
+                                                    ///  CODE STARTS FROM HERE    ///
+
+
+
+void solve()
+{   
+   int n;
+   cin>>n;
+   int mn=min(1,n);
+   int mx=max(1,n);
+   ll sum=0;
+   for(int i=mn;i<=mx;i++)
+   {
+       sum+=i;
+   }
+   cout<<sum<<el;
+
+
+   
+}
+
+int main()
+{
+    optimise;
+    solve();
+    
+
+}
+
+// problem link : https://acm.timus.ru/problem.aspx?space=1&num=1068
